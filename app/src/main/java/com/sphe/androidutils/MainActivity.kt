ba08082703b14894c7137283c0eabffaec56d21c
sphelele95 <sphelele@ikhokha.com>
2020-05-02 20:46:08 +0200

    - ✨ implemented phonecodepicker  in sample app

--- app/src/main/java/com/sphe/androidutils/MainActivity.kt
+++ app/src/main/java/com/sphe/androidutils/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.sphe.androidutils
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        const val TAG = "@MainActivity"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        text.text = String.format("%s | %s",countryPicker.selectedCountryName,countryPicker.selectedCountryCode)
+        countryPicker.registerPhoneNumberTextView(edtText)
+
+        countryPicker.setOnCountryChangeListener {country ->
+            text.text = String.format("%s | %s",country.name,country.phoneCode)
+        }
     }
 }
